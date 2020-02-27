4c356a03534742d4916ac7037cdb06c2f05103ba
yiteng.liu <yiteng.liu@11bee.com>
2020-02-27 21:05:32 +0800

    small

---
+++ src/main/resources/server-script-template.js
@@ -0,0 +1,6 @@
+var hashmap=${modidhashpairs}
+var Loader = Java.type("cpw.mods.fml.common.Loader");
+var DigestUtils = Java.type("org.apache.commons.codec.digest.DigestUtils");
+function checkHash() {
+
+}
